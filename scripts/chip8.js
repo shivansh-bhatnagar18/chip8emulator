06de28227d18c9c1e2c8e889098a288c15aaedd7
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2023-08-21 10:10:32 +0000

    some minor changes

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -1,12 +1,12 @@
-import Renderer from "Renderer.js";
-import Mapping from "Mapping.js";
-import Speaker from "Speaker.js";
-import CPU from "control.js";
+import Renderer from "./Renderer.js";
+import Mapping from "./Mapping.js";
+import Speaker from "./Speaker.js";
+import CPU from "./control.js";
 
 const renderer = new Renderer(10);
 const mapping = new Mapping();
 const speaker = new Speaker();
-const cpu = new CPU(renderer, keyboard, speaker)
+const cpu = new CPU(renderer, mapping, speaker)
 
 let loop, fps = 60, fpsInterval, startTime, currentTime, pastTime, elapsed;
 
